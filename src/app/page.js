45b577a67d01d531c6c31abbe2f1c464e0bd5577
Gi-Palumbo <125066782+Gi-Palumbo@users.noreply.github.com>
2023-08-25 10:44:25 -0300

    Add files via upload

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,46 @@
-import Titulo from '@/components/titles'
+import Titulo from '@/components/titles.jsx'
 import Image from 'next/image'
+import CardFilme from '@/components/CardName'
 
 export default function Home() {
+  const filmes=[
+    {
+      titulo: "Fourth Wing" ,
+      autor:"Rebecca Yarros",
+      nota:4.66,
+      poster:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1666994927i/61431922.jpg"
+    },
+    {
+      titulo: "Happy Place" ,
+      autor:"Emily Henry",
+      nota:4.12,
+      poster:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1660145160i/61718053.jpg"
+    },
+    {
+      titulo: "Yellow Face" ,
+      autor:"R.F Kuang",
+      nota:3.98,
+      poster:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1687082673i/59357120.jpg"
+    },
+    {
+      titulo: "The Stolen Heir" ,
+      autor:"Holly Black",
+      nota:4.05,
+      poster:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1654625328i/61198133.jpg"
+    },
+    {
+      titulo: "Hell Bent" ,
+      autor:"Leigh Bardugo",
+      nota:4.18,
+      poster:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1648744962i/34019110.jpg"
+    }
+  ]
+ 
+  
+
+
+
+
   return (
 <>
     <nav className="bg-cyan-950 p-2 flex gap-10  items-end ">
@@ -15,10 +54,17 @@ export default function Home() {
     </nav>
 
     <Titulo>Best books of 2023</Titulo>
+
+    <section className="flex flex-wrap gap-10 ml-14 ">
+    {filmes.map( filme => <CardFilme filme={filme}/>)}
+    </section>
+
+
     <Titulo>Trending</Titulo>
 
     <Titulo texto="Best Books of 2023" />
     <Titulo texto="Trending" />
+
   </>
   )
 }
---
+++ src/components/titles.jsx
@@ -0,0 +1,6 @@
+export default function Titulo({children}){
+    return(
+        <h2 className="text-2xl border-l-8 border-emerald-400 m-8 pl-2 leading-6">
+            {children}</h2>
+    )
+}
